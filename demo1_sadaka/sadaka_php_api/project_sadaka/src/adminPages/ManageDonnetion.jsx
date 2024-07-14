c323999c06245beea7b68a1bf735a9738c47c9db
Raihan-Raju <idbraihan58@gmail.com>
2024-07-14 23:00:25 +0600

    project

---
+++ demo1_sadaka/sadaka_php_api/project_sadaka/src/adminPages/ManageDonnetion.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import AdminNavbar from '../adminComponents/AdminNavbar';
+import AdminSidebar from '../adminComponents/AdminSidebar';
+import AdminFooter from '../adminComponents/AdminFooter';
+import AdminSlider from '../adminComponents/AdminSlider';
+import {  NavLink } from "react-router-dom";
+const ManageDonnetion = () => {
+    return (
+        <>
+          <AdminNavbar />
+          <AdminSidebar />
+
+          <div id="page-wrapper">
+                <div id="page-inner">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h1 className="page-head-line">DONNETION:</h1>
+                            <AdminSlider />
+                            <NavLink to="/addDonnetion"><button className="btn btn-info btn-lg ">Add_Donnetion</button></NavLink>
+                        </div>
+           
+                        <>
+  {/* Hello world */}
+  <div className="row">
+    <div className="col-md-12">
+      {/*   Kitchen Sink */}
+      <div className="panel panel-default">
+        <div className="panel-heading">Manage_Donnetion</div>
+        <div className="panel-body">
+          <div className="table-responsive">
+            <table className="table table-striped table-bordered table-hover">
+              <thead>
+                <tr>
+                  <th>#No</th>
+                  <th>Amount</th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Email</th>
+                  <th>Phone</th>
+                  <th>Address</th>
+                  <th>Note</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>2000</td>
+                  <td>Otto</td>
+                  <td>mdo</td>
+                  <td>@mdo</td>
+                  <td>027455718774</td>
+                  <td>Lalbagh</td>
+                  <td>good</td>
+                  <td>Edit/Delete</td>
+                </tr>
+
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      {/* End  Kitchen Sink */}
+    </div>
+   
+  </div>
+</>
+
+                    </div>
+                 </div>
+                 <AdminFooter/>
+             </div>
+            
+          
+        </>
+    );
+};
+
+export default ManageDonnetion;
